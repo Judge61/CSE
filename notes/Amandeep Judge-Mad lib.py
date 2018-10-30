29c86bff16e56774c12de89dd03cec10fc8fa9ec
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-10-30 09:46:14 -0700

    initial upload

---
+++ notes/Amandeep Judge-Mad lib.py
@@ -0,0 +1,5 @@
+variables:
+print("As a %s you have to be %s and do stuff like %s, %s, and %s" % (Adjective, Adjective, Verb, Verb, Verb))
+
+Also, the biggest tip is to be %s because everyone wants a %s not some %s person.
+The three important factors to being successful as a ------- (repeat) to be ------, --------, and -----.
--- notes/Hello World.py
+++ notes/Hello World.py
@@ -1,3 +1,4 @@
+"""
 print("Hello World!")
 
 # This is a comment. This has no effect on the code
@@ -52,11 +53,43 @@ hidden_age = real_age + 5
 print("This is your real age: %d" % hidden_age)
 
 
+"""
+
+# Functions
+def say_it():
+    print("Hello World")
 
 
+say_it()
+say_it()
+
+# f(x) = 2x + 3
+def f(x):
+    print(2*x + 3)
+
+f(1)
+f(5)
+f(5000)
+
+# Distance Formula
+def distance(x1, y1, x2, y2):
+    dist = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+    print(dist)
+
+distance(0, 0, 3, 4)
+distance(0, 0, 5, 12)
+
+# Loops
+for i in range(5):  # This gives the numbers 0 through 4
+    say_it()
 
 
+for i in range(10):
+    print(i+1)
 
+for i in range(5)
+    f(i)
+    
 
 
 
