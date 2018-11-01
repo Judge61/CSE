5a4a2ca3fb01e0f3577f190593243e7c5dfcf327
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-11-01 09:43:39 -0700

    upload initial

--- notes/Amandeep Judge-Mad lib.py
+++ notes/Amandeep Judge-Mad lib.py
@@ -1,5 +1,13 @@
 variables:
-print("As a %s you have to be %s and do stuff like %s, %s, and %s" % (Adjective, Adjective, Verb, Verb, Verb))
 
-Also, the biggest tip is to be %s because everyone wants a %s not some %s person.
-The three important factors to being successful as a ------- (repeat) to be ------, --------, and -----.
+plural_noun=
+Adjective=
+Personality=
+
+input ("As a %s you have to be %s and do stuff like %s, %s, and %s" % (plural noun, Adjective, Verb, Verb, Verb))
+print("Also, the biggest tip is to be %s because everyone wants a %s not some %s person" % (Adjective, Adjective, Adjective))
+print("The three important factors to being successful as a %s to be %s and %s" % (Adjective, personality, personality))
+
+
+
+
