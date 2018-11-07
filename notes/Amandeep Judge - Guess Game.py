ab3554d0d6c76e3e1c6429768a7a9b379a0b8eb9
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-11-07 09:44:45 -0800

    initial upload

--- notes/Amandeep Judge - Guess Game.py
+++ notes/Amandeep Judge - Guess Game.py
@@ -1,5 +1,15 @@
 import random
-
-print
-
-
+print(random.randint(0, 10))
+playing = True
+guesses_left = 5
+while guesses_left > 0 and playing:
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
--- notes/Amandeep Judge-Mad lib.py
+++ notes/Amandeep Judge-Mad lib.py
@@ -3,17 +3,13 @@ Adjective1 = input("Put in an adjective : ")
 Verb1 = input("Put in a verb : ")
 Verb2 = input("Put in a verb : ")
 Verb3 = input("Put in a verb : ")
-place1 = input ("Put in a place : ")
-place2 = input ("Put in a place : ")
-place3 = input ("Put in a place : ")
+place1 = input("Put in a place : ")
+place2 = input("Put in a place : ")
+place3 = input("Put in a place : ")
 feeling1 = input("Put in a feeling : ")
 feeling2 = input("Put in a feeling : ")
-material = input("Put in a type material :  ")
+thing = input("Put in a thing or an object :  ")
 
 print("As a %s you have to be %s and do stuff like %s, %s, and %s" % (noun, Adjective1, Verb1, Verb2, Verb3))
 print("You get to go to places like %s, %s, and %s. " % (place1, place2, place3))
-print("You might feel %s or %s but remember to not forget about the glorious %s" % (feeling1, feeling2, material))
-
-
-
-
+print("You might feel %s or %s but remember to not forget about the glorious %s" % (feeling1, feeling2, thing))
