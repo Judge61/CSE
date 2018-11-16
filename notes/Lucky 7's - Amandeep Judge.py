33e7829b959ca298bd2f1605d5191f41e2ffd06c
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-11-16 09:45:01 -0800

    initial upload

--- notes/Lucky 7's - Amandeep Judge.py
+++ notes/Lucky 7's - Amandeep Judge.py
@@ -3,6 +3,8 @@ playing = True
 
 money = int(15)
 rounds = 0
+max_money = int(15)
+max_rounds = 0
 
 while money > 0:
     D6 = random.randint(1, 6)
@@ -13,6 +15,9 @@ while money > 0:
         print("You rolled a %s and %s" % (D1, D6))
         print(money)
         rounds += 1
+        if max_money < money:
+            max_money = money
+            max_rounds = rounds
     elif roll > 7:
         money -= 1
         print("You rolled a %s and %s" % (D1, D6))
@@ -26,6 +31,8 @@ while money > 0:
 
 
 print("You rolled %s rounds" % rounds)
+print("Your maximum amount was %s" % max_money)
+print("You should have stopped at round %s" % max_rounds)
 
 
 
