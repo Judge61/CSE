76f0ec924b67c5806635ccef84990a007afac5a0
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-11-28 09:44:37 -0800

    initial upload

---
+++ notes/Amandeep Judge- Mad Libs 2.py
@@ -0,0 +1,10 @@
+List1 = ["As a %s you have to be %s and do stuff like %s, %s, and %s. You get to go to places like %s, %s, and %s." 
+        "You might feel %s or %s but remember to not forget about the glorious %s"]
+
+List1.append(input())
+
+"As a %s you have to be %s and do stuff like %s, %s, and %s"
+"You get to go to places like %s, %s, and %s. "
+"You might feel %s or %s but remember to not forget about the glorious %s"
+
+
