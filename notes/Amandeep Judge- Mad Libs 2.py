065550f30b22657edc8d5c2946f901de3a4c3aa6
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-11-29 09:44:59 -0800

    initial upload

--- notes/Amandeep Judge- Mad Libs 2.py
+++ notes/Amandeep Judge- Mad Libs 2.py
@@ -1,10 +1,8 @@
-List1 = ["As a %s you have to be %s and do stuff like %s, %s, and %s. You get to go to places like %s, %s, and %s." 
+List1 = ["As a %s you have to be Blank1 and do stuff like Blank2, Blank3, and %s. You get to go to places like %s, %s, and %s." 
         "You might feel %s or %s but remember to not forget about the glorious %s"]
 
-List1.append(input())
-
-"As a %s you have to be %s and do stuff like %s, %s, and %s"
+List1.append(input("As a %s you have to be %s and do stuff like %s, %s, and %s"
 "You get to go to places like %s, %s, and %s. "
 "You might feel %s or %s but remember to not forget about the glorious %s"
-
+))
 
