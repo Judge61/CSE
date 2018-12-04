d68ea2eaf415904804bffe6970e9d6488c673924
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-12-04 09:44:48 -0800

    initial upload

--- notes/Challanges.py
+++ notes/Challanges.py
@@ -1,2 +1,8 @@
-def challenge1(firstname, lastname):
-    return (John Doe)
+def challenge_1(firstname, lastname):
+    return lastname, firstname
+
+
+print(challenge_1("Amandeep", "Judge"))
+
+num: int(input("Enter a number"))    # use this whenever you want them to answer something while it is running
+
