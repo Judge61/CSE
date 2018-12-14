61262faa258eae25226baaaaf211694fb6ec871b
Judge61 <42385606+Judge61@users.noreply.github.com>
2018-12-14 10:05:32 -0800

    initial upload

---
+++ notes/Amandeep Judge - Python.py
@@ -0,0 +1,28 @@
+def challenge_1(firstname, lastname):
+    return lastname, firstname
+
+
+print(challenge_1("Amandeep", "Judge"))
+
+
+def challenge_2(number):
+    if number % 2 == 0:
+        print('The number is even')
+    else:
+        print('The number is odd')
+
+
+print(challenge_2(8))
+
+
+def challenge_4(number):
+    if number > 0:
+        print("The number is positive")
+    elif number == 0:
+        print("The number is 0")
+    else:
+        print("The number is negative")
+
+
+print(challenge_4(-9))
+
