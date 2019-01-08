ceceb30250e956b833327c2e9b21e2d706800f82
Judge61 <42385606+Judge61@users.noreply.github.com>
2019-01-08 09:45:20 -0800

    initial upload

---
+++ Semester Notes.py
@@ -0,0 +1,20 @@
+print("Hello World")
+
+# single line comment
+
+cars = 5
+driving = True
+
+
+print("I have %s cars" % cars)
+
+age = input("How old are you")
+print(age + "????You should not lie about your age")
+
+colors = ["blue", "green", "orange", "pink", "magenta"]
+colors.append("cyan")
+print(colors)
+colors.pop(2)
+print(colors)
+print(colors[2])
+print(len(colors))
