679d2d669468fe21a16db2c18296702fd5a95d4b
Judge61 <42385606+Judge61@users.noreply.github.com>
2019-01-11 10:39:42 -0800

    initial upload

--- notes/Amandeep Judge - Hangman.py
+++ notes/Amandeep Judge - Hangman.py
@@ -1,7 +1,5 @@
-List = ["Oh", "no!" "The", "goblins", "are", "coming", "to", "burn", "this", "village!"]
-import_random
+import random
 
+List = ["Oh", "no!", "The", "goblins", "are", "coming", "to", "burn", "this", "village!"]
+print(random.choices(List, k=1))
 guesses = 8
-
-
-
