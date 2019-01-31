a56d354f364c6c8d26c8bdcd46a03fc4a1f62acd
Judge61 <42385606+Judge61@users.noreply.github.com>
2019-01-31 09:43:14 -0800

    initial upload

--- notes/world_map - Amandeep Judge.py
+++ notes/world_map - Amandeep Judge.py
@@ -1,19 +1,19 @@
 world_map = {
     'R19A': {
-        'Name': "Mr. Wiebe's room",
-        'Description': "This is the classroom you are in right now. "
+        'NAME': "Mr. Wiebe's room",
+        'DESCRIPTION': "This is the classroom you are in right now. "
                        "Now, there are two doors on the north wall.",
-        'Paths': {
-            'North': "PARKING_LOT"
+        'PATHS': {
+            'NORTH': "PARKING_LOT"
 
 }
 
     },
     'PARKING_LOT': {
-        'Name': "The North Parking Lot",
-        'Description': "There are a couple of cars parked here",
-        'Paths': {
-            'South': 'R19A'
+        'NAME': "The North Parking Lot",
+        'DESCRIPTION': "There are a couple of cars parked here",
+        'PATHS': {
+            'SOUTH': 'R19A'
         }
     }
 }
@@ -22,8 +22,21 @@ world_map = {
 # controller
 playing = True
 current_node = world_map['R19A']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
 while playing:
-    print(current_node{})
+    print(current_node['NAME'])
+    print(current_node['DESCRIPTION'])
     command = input(">_")
     if command.lower() in {'q', 'quit', 'exit'}:
         playing = False
+    elif command.upper() in directions:
+       try:
+        room_name = current_node['PATHS'][command.upper()]
+        current_node = world_map[room_name]
+       except KeyError:
+        print("I can't go that way")
+    else:
+        print("Command not found")
+
+
+
