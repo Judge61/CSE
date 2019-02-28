f7e1985415e2b2e58e0100854c0c2c5d21dfabb5
Judge61 <42385606+Judge61@users.noreply.github.com>
2019-02-28 09:37:10 -0800

    initial upload

---
+++ notes/inheritance.py
@@ -0,0 +1,42 @@
+class Vehicle(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Car(Vehicle):
+    def __init__(self, name, mileage):
+        super(Car, self).__init__(name)
+        self.engine_status = False
+        self.fuel = 100
+        self.mileage = mileage
+
+    def start_engine(self):
+        self.engine_status = True
+        print("You turn the key and the car turns on")
+
+    def move_forward(self):
+        self.fuel -= 1
+        print("The car moves forward")
+
+    def turn_left(self):
+        self.fuel -= 1
+        print("HE IS MAKING A LEFT TURN!")
+
+    def turn_off(self):
+        self.engine_status = False
+        print("You turned off the car")
+
+
+class Impala(Car):
+    def __init__(self):
+        super(Impala, self).__init__('Impala', 25)
+
+
+aman_car = Impala()
+aman_car.start_engine()
+aman_car.move_forward()
+aman_car.turn_left()
+aman_car.move_forward()
+aman_car.turn_off()
+
+
