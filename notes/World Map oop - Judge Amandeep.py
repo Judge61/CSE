14a1bfc3204865fb6196b14d34e66965676feeb9
Judge61 <42385606+Judge61@users.noreply.github.com>
2019-03-26 09:44:19 -0700

    initial upload

--- notes/World Map oop - Judge Amandeep.py
+++ notes/World Map oop - Judge Amandeep.py
@@ -1,6 +1,8 @@
 class Room(object):
-    def __init__(self, name, description, north=None, east=None, south=None, west=None):
-
+    def __init__(self, name, description, north=None, east=None, south=None, west=None, characters=None):
+        if characters is None:
+            characters = []
+        self.characters = characters
         self.name = name
         self.north = north
         self.east = east
@@ -59,11 +61,6 @@ class Bookexperiments (Item):
         super(Bookexperiments, self).__init__("Book: Experiments")
 
 
-class Serpentsblood (Item):
-    def __init__(self):
-        super(Serpentsblood, self).__init__("serpent's blood")
-
-
 class Cauldron (Item):
     def __init__(self):
         super(Cauldron, self).__init__("Cauldron")
@@ -101,7 +98,16 @@ class Lantern(Item):
         print("Your lantern is glowing and you can finally see!")
 
 
+def character(object):
+    def __init__(self, name):
+        self.name = name
+
+
+lich1 = character("Bri the Lich")
+troll1 = character("Troll")
+
 # You will not be able to use any of the items even if you try. Only the scientist can!
+
 
 class Player(object):
     def __init__(self, starting_location):
@@ -136,9 +142,9 @@ Room_2 = Room("Blue House", "The entrance leads right to the living room."
 Room_3 = Room("Kitchen", "There are no windows but there is a pantry to your north."
                          " You may find some valuable things in the pantry.", 'Room_4', None, None, 'Room_2')
 
-Room_4 = Room("Pantry", "Upon opening the door, you found a bottle."
-                        " There is a -------------."
-                        " This is perfect if your health is at risk."
+Room_4 = Room("Pantry", " Upon opening the door, There are only two items in here."
+                        " There is a phoenix feather."
+                        " There is a bottle that says: Venom: Boomslang"
                         " To the south and to the east, there are two rooms with the names: Loot_room2 or Loot_room1."
                         " Which room will you go to?", None, 'Room_6', 'Room_5', 'Room_3')
 
@@ -146,8 +152,9 @@ Room_5 = Room("Loot_room2", "You chose the wrong room!"
                             " This is an empty room.", 'Room_4', None, None, None)
 
 Room_6 = Room("Loot_room1", "You chose the right room!"
-                            " In this room there are drawers."
+                            " In this room there are many drawers."
                             " In the drawers there is a pocket watch."
+                            " There is a huge diamond."
                             " On the back it reads: Brianna Lich "
                             " To the south there is the owner’s room.", None, None, 'Room_7', 'Room_4')
 
@@ -159,12 +166,13 @@ Room_7 = Room("Owner's_room", "The room is very dark but there is a lantern in t
                               'Room_12', None)
 
 Room_8 = Room("Garden", "There are rows and rows of flowers."
-                        " There is one special purple flower named: Supernova to your left."
+                        " There is one special/violet flower named: Supernova."
+                        " There is a four leaf clover too."
                         " There are two buildings in front of you"
                         " To the east, there is a science lab and to the south there is a mini"
                         "house where the owner is waiting.", None, 'Room_9', 'Room_10', 'Room_7')
 
-Room_9 = Room("Science_lab",  "There are a lot of potions."
+Room_9 = Room("Science_lab",  "There is a beaker, a pan, gloves, a couldren, and droppers."
                               " Looks like the owner or your master was a scientist!", None, None, None, 'Room_8')
 
 Room_10 = Room("Owner's_mini_house_entry", "You are led straight to the living room."
@@ -176,17 +184,17 @@ Room_10 = Room("Owner's_mini_house_entry", "You are led straight to the living r
 Room_11 = Room("Bedroom", "You have made it!"
                           " Let's see if you have all the things the master needs.", None, None, None, 'Room_10')
 
-Room_12 = Room("Dungeon", "There is a sword and a lightsaber.\n"
+Room_12 = Room("Dungeon", "There is a sword."
                           " To the south: Lich room.", 'Room_7', None, 'Room_13', None)
 
 Room_13 = Room("Lich_room", "Fight the Lich!!!!"
-                            " Next room: South: Troll room", 'Room_12', None, 'Room_14', None)
+                            " Next room: South: Troll room", 'Room_12', None, 'Room_14', None, "Bri the Lich")
 
 Room_14 = Room("Troll_room", "Fight the troll."
-                             " South: where the serpent's blood is.", 'Room_13', None, 'Room_15', None)
+                             " South: where the dragon's blood is.", 'Room_13', None, 'Room_15', None)
 
-Room_15 = Room("The_Emerald", "The dragon's blood is east of you!"
-                              " Take it and go to the garden to see where to go next!", 'Room_14', None, None, None)
+Room_15 = Room("Loot_room3", "The dragon's blood and the pixie dust is east of you!"
+                             " Take it and go to the garden to see where to go next!", 'Room_14', None, None, None)
 
 player = Player(Room_1)
 
