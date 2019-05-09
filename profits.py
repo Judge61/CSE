b40e63d5bd95a008aae82f552c2bfba872d475ef
Judge61 <42385606+Judge61@users.noreply.github.com>
2019-05-09 09:45:45 -0700

    initial upload

--- profits.py
+++ profits.py
@@ -3,5 +3,7 @@ import csv
 with open("Sales_Records.csv", 'r') as old_csv:
     reader = csv.reader(old_csv)
     for row in reader:
-        old_number = (row[0])
+        old_number = (row[2])
         print(old_number)
+# Vegetables, beverages, office supplies, personal care, fruits, cereal, cosmetics, household, clothes, baby food, meat,
+# snack
